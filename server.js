c630d12ad1e04d9b01ee30fb1018373aa95b3f5e
bpmcdonell <73715095+bpmcdonell@users.noreply.github.com>
2023-03-13 22:39:41 -0400

    done

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const routes = require('./routes');
 const sequelize = require('./config/connection');
 
 const app = express();
-const PORT = process.env.PORT || 3306;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -16,6 +16,4 @@ sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-});
+
